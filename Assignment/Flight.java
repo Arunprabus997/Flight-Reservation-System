4ecd52440e1f004f9a9f5ccf399c279b83123b4b
Arunprabu <harikrishs932@gmail.com>
2021-04-14 19:38:38 +0530

    Testcase 4

--- Assignment/Flight.java
+++ Assignment/Flight.java
@@ -1,14 +1,23 @@
 package com.FRS;
-
 public class Flight {
     private String flightNo;
     private String airlines;
     private int capacity;
     private int noOfSeatsBooked;
-    public String getFlightDetails(){
+    //Parameterised Constructor
+    public Flight(String flightNo, String airlines, int capacity, int noOfSeatsBooked) {
+        this.flightNo = flightNo;
+        this.airlines = airlines;
+        this.capacity = capacity;
+        this.noOfSeatsBooked = noOfSeatsBooked;
+    }
+    //Method to get flight details
+    public String getFlightDetails() {
         return "Flight No : "+ flightNo+" and "+"Airlines :"+airlines;
     }
-    public boolean checkAvailabilityOfSeat(){boolean x;
+    //method to check availability of seat
+    public boolean checkAvailabilityOfSeat(){
+        boolean x;
         if (noOfSeatsBooked < capacity){
             x = true;
         }else{
@@ -16,51 +25,33 @@ public class Flight {
         }
         return x;
     }
-    public void incrementAvailableCounter(){
-        noOfSeatsBooked +=noOfSeatsBooked;
+    //Method to increment no of seats booked
+    public void incrementAvailableCounter() {
+        this.noOfSeatsBooked++;
+        this.capacity -=1;
     }
-
-    public Flight(String flightNo, String airlines, int capacity, int noOfSeatsBooked) {
-        this.flightNo = flightNo;
-        this.airlines = airlines;
-        this.capacity = capacity;
-        this.noOfSeatsBooked = noOfSeatsBooked;
-    }
-    public  Flight(Flight flight){
-        this.flightNo = flight.flightNo;
-        this.airlines = flight.airlines;
-        this.capacity = flight.capacity;
-        this.noOfSeatsBooked = flight.noOfSeatsBooked;
-    }
-
+    // getter and setters for attributes
     public String getFlightNo() {
         return flightNo;
     }
-
     public void setFlightNo(String flightNo) {
         this.flightNo = flightNo;
     }
-
     public String getAirlines() {
         return airlines;
     }
-
     public void setAirlines(String airlines) {
         this.airlines = airlines;
     }
-
     public int getCapacity() {
         return capacity;
     }
-
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
-
     public int getNoOfSeatsBooked() {
         return noOfSeatsBooked;
     }
-
     public void setNoOfSeatsBooked(int noOfSeatsBooked) {
         this.noOfSeatsBooked = noOfSeatsBooked;
     }
--- Assignment/Main.java
+++ Assignment/Main.java
@@ -1,29 +1,28 @@
 package com.FRS;
 public class Main {
-
     public static void main(String[] args) {
-        // write your code here
+	// write your code here
         Passenger passanger = new Passenger("arun","9876543210","harikrishs932@gmail.com",
                 "sugar mill","cuddalore","tamilnadu");
         System.out.println(passanger.getAddressDetails());
         System.out.println(passanger.getContactDetails());
-        System.out.println(passanger.id);
+        System.out.println(passanger.getId());
 
         Flight flight1 = new Flight("1","jetlag",100,4);
         System.out.println(flight1.getFlightDetails());
         System.out.println(flight1.checkAvailabilityOfSeat());
-        RegularTicket rt1= new RegularTicket("278dg","banglore","london","26/7 9.00",
-                "27/7 7.00",20,30000.0f,false,"food",flight1,passanger);
+        RegularTicket rt1= new RegularTicket("278dg","banglore","london","26/7 3.00",
+                "26/7 9.00",20,30000.0f,false,"food",flight1,passanger);
         rt1.checkStatus();
-        Passenger sunil = new Passenger(passanger);
         Flight flight2 = flight1;
-        System.out.println(sunil.id);
-        TouristTicket tt1 = new TouristTicket("2222","banglore","london","26/7 9.00",
-                "27/7 7.00",20,30000.0f,false,"Taj",null, flight2, sunil);
+        System.out.println(passanger.getId());
+        TouristTicket tt1 = new TouristTicket("2222","banglore","london","26/7 9",
+                        "27/7 7",20,30000.0f,false,"Taj",null, flight2, passanger);
 
         System.out.println(flight1.getFlightNo());
         flight1.setFlightNo("ad134");
         System.out.println(flight1.getFlightNo());
 
+        System.out.println(rt1.durationOfJourney());
     }
 }
